db358700adbacb1d8c7b94cf6ee564a13fe1412b
hulyak <hulyakarakayaa06@gmail.com>
2021-07-11 12:35:58 -0700

    install dependencies

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,15 +4,17 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 // Initalize Firebase.
-var firebaseConfig = {
-  apiKey: "AIzaSyCOq3qrkFz2XJK_-2kiUB-G3bhm1DxxLHo",  
-  authDomain: "tcl-28-shopping-list.firebaseapp.com",
-  projectId: "tcl-28-shopping-list",
-  storageBucket: "tcl-28-shopping-list.appspot.com",
-  messagingSenderId: "499603825685",
-  appId: "1:499603825685:web:e1385df4cdcd43ec1a9a1c"
+const firebaseConfig = {
+  apiKey: 'AIzaSyCOq3qrkFz2XJK_-2kiUB-G3bhm1DxxLHo',
+  authDomain: 'tcl-28-shopping-list.firebaseapp.com',
+  projectId: 'tcl-28-shopping-list',
+  storageBucket: 'tcl-28-shopping-list.appspot.com',
+  messagingSenderId: '499603825685',
+  appId: '1:499603825685:web:e1385df4cdcd43ec1a9a1c',
 };
 
 let fb = firebase.initializeApp(firebaseConfig);
 
-export { fb };
+const firestore = firebase.firestore();
+
+export { fb, firestore };
