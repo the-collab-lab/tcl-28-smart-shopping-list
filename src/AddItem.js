bafb87a1e76f561ab7d719fa51f4d3995783f599
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-07-11 16:17:36 -0400

    connect button handleClick with firebase

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,13 +1,13 @@
 import { firestore } from './lib/firebase';
 
-const AddItem = () => (item) => {
+const AddItem = () => {
   // get a reference to the Firestore document
   const handleClick = async () => {
     const itemTemplate = {
       name: 'New item',
       date: Date.now(),
     };
-    await firestore().collection('items').add(itemTemplate);
+    await firestore.collection('items').add(itemTemplate);
   };
 
   return <button onClick={handleClick}>Add an item</button>;
