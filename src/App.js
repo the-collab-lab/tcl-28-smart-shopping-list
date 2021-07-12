d70cc5566e4a9dbca795e6683bb731a99f4e16ba
Kelsey Siman <ksiman14@gmail.com>
2021-07-12 12:01:45 -0600

    added routes and shell footer component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Router, Route } from 'react-router-dom';
+import AddItem from './AddItem';
+import ItemsList from './ItemsList';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route path="/add" component={AddItem} />
+        <Route path="/list" component={ItemsList} />
+      </Router>
+      <Footer />
     </div>
   );
 }
