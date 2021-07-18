ef4313f391f77025d4c3d60269b414ebd2212fd9
hulyak <hulyakarakayaa06@gmail.com>
2021-07-18 08:18:16 -0700

    generate a new token for the new user

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import AddItem from './AddItem';
 import ItemsList from './ItemsList';
 import Footer from './Footer';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={ItemsList} />
+        <Route exact path="/" component={Home} />
         <Route path="/add" component={AddItem} />
         <Route path="/list" component={ItemsList} />
       </Switch>
