cfee15857ada85b0666193d97be8570e6953f24c
Kelsey Siman <ksiman14@gmail.com>
2021-07-18 09:43:01 -0600

    if user already has list token, home redirects them to the list

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import getToken from './lib/tokens';
 import { useHistory } from 'react-router-dom';
 
 const Home = () => {
   const history = useHistory();
 
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    if (token) history.push('/list');
+  });
+
   const createToken = () => {
     const token = getToken();
     localStorage.setItem('token', JSON.stringify(token));
     history.push('/list');
   };
+
   return (
     <div>
       <h1>Welcome to Your Smart Shopping list!</h1>
@@ -17,7 +23,7 @@ const Home = () => {
       <p>-or-</p>
       <p>Join an existing shopping list by entering a three word token.</p>
       <form>
-        <label HTMLFor="token">Share token</label>
+        <label htmlFor="token">Share token</label>
         <input type="text" name="token" />
         <button type="submit"> Join an existing list</button>
       </form>
