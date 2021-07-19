1c450f1fa182fd02f8497f52a7caae0155550de5
Isa Abutaa <isaabutaa@gmail.com>
2021-07-19 19:05:28 -0400

    created state value for frequency and handleChange function for inputs

--- src/AddItem.js
+++ src/AddItem.js
@@ -3,41 +3,72 @@ import { firestore } from './lib/firebase';
 
 const AddItem = () => {
   const [item, setItem] = useState('');
+  const [frequency, setFrequency] = useState(0);
+  const [lastPurchasedDate, setLastPurchasedDate] = useState(null);
+
+  const token = JSON.parse(localStorage.getItem('token'));
 
   // get a reference to the Firestore document
   const handleClick = async () => {
     const itemTemplate = {
-      name: 'New item',
+      name: item,
+
       date: Date.now(),
     };
     await firestore.collection('items').add(itemTemplate);
   };
 
+  const handleChange = (e) => {
+    if (e.target.name === 'item') {
+      setItem(e.target.value);
+      console.log(item);
+    } else if (e.target.name === 'frequency') {
+      setFrequency(e.target.value);
+      console.log(frequency);
+    }
+  };
+
   return (
     <div>
       <h1>Smart Shopping List</h1>
-      <form>
-        <label for="item">
+      <form onSubmit={handleClick}>
+        <label htmlFor="item">
           Item Name:
           <input
             type="text"
             name="item"
             value={item}
             placeholder="Type item here"
+            onChange={handleChange}
           />
         </label>
         <fieldset>
           <legend>How soon will you buy this again?</legend>
-          <label for="frequency">
-            <input type="radio" name="frequency" value={7}></input>
+          <label htmlFor="frequency">
+            <input
+              type="radio"
+              name="frequency"
+              value={7}
+              onChange={handleChange}
+            ></input>
             Soon
           </label>
-          <label for="frequency">
-            <input type="radio" name="frequency" value={14}></input>
+          <label htmlFor="frequency">
+            <input
+              type="radio"
+              name="frequency"
+              value={14}
+              onChange={handleChange}
+            ></input>
             Kind of soon
           </label>
-          <label for="frequency">
-            <input type="radio" name="frequency" value={30}></input>
+          <label htmlFor="frequency">
+            <input
+              type="radio"
+              name="frequency"
+              value={30}
+              onChange={handleChange}
+            ></input>
             Not soon
           </label>
         </fieldset>
