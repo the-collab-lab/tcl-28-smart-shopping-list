5e739ee1f1e618a94176329890beda2c2c7a93de
Isa Abutaa <isaabutaa@gmail.com>
2021-07-19 19:29:16 -0400

    added alert notification that confirms when the item has been added to the list, and used history.push() to redirect user to list page

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import getToken from './lib/tokens';
 import { firestore } from './lib/firebase';
+import { useHistory } from 'react-router-dom';
 
 const AddItem = () => {
   const [item, setItem] = useState('');
   const [frequency, setFrequency] = useState(0);
   const [lastPurchasedDate, setLastPurchasedDate] = useState(null);
+  const history = useHistory();
 
   // For testing purposes to make sure we can push new item to Firestore
   useEffect(() => {
@@ -23,6 +25,9 @@ const AddItem = () => {
       lastPurchasedDate,
     };
     await firestore.collection('items').add(itemTemplate);
+
+    alert(`Successfully added ${item} to your list!`);
+    history.push('/list');
   };
 
   const handleChange = (e) => {
