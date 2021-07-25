5ebc0c018751ebd4001a8b1f8e5dcb2c9d6c3890
hulyak <hulyakarakayaa06@gmail.com>
2021-07-25 16:21:14 -0700

    create a state for shared token

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import getToken from '../lib/tokens';
 import { useHistory } from 'react-router-dom';
 import { firestore } from '../lib/firebase';
 
 const Home = () => {
   const history = useHistory();
+  const [sharedToken, setSharedToken] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) history.push('/list');
   });
 
-  const createToken = async () => {
+  const createToken = async (sharedToken) => {
+    if (sharedToken) {
+      const token = await getToken();
+    }
     const token = getToken();
     localStorage.setItem('token', token);
     await firestore.collection('tokens').doc(token).set({});
@@ -21,12 +25,17 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome to Your Smart Shopping list!</h1>
-      <button onClick={createToken}>Create a new list</button>
+      <button onClick={() => createToken()}>Create a new list</button>
       <p>-or-</p>
       <p>Join an existing shopping list by entering a three word token.</p>
-      <form>
+      <form onSubmit={() => createToken(sharedToken)}>
         <label htmlFor="token">Share token</label>
-        <input type="text" name="token" />
+        <input
+          type="text"
+          name="token"
+          value={sharedToken}
+          onChange={(e) => setSharedToken(e.target.value)}
+        />
         <button type="submit"> Join an existing list</button>
       </form>
     </div>
