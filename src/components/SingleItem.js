ee8f72c09e2182e0ba8203afc7ea0497a143190c
Isa Abutaa <isaabutaa@gmail.com>
2021-08-17 20:34:34 -0400

    add accessibility for checkbox

--- src/components/SingleItem.js
+++ src/components/SingleItem.js
@@ -20,14 +20,19 @@ const SingleItem = (props) => {
   );
 
   let bgColor;
+  let aria;
   if (daysPassedSincePurchase <= daysUntilPurchase) {
     bgColor = 'green';
+    aria = 'buy soon';
   } else if (daysPassedSincePurchase > 7 && daysPassedSincePurchase < 30) {
     bgColor = 'orange';
+    aria = 'buy kind of soon';
   } else if (daysPassedSincePurchase > 30 && daysPassedSincePurchase < 40) {
     bgColor = 'red';
+    aria = 'wait a while before buying';
   } else if (daysPassedSincePurchase >= 40) {
     bgColor = 'gray';
+    aria = "you haven't bought this in a long time";
   }
 
   const updateIsPurchased = async (id) => {
@@ -83,7 +88,7 @@ const SingleItem = (props) => {
 
   return (
     <div style={{ backgroundColor: bgColor }}>
-      <label htmlFor={name}>
+      <label htmlFor={name} aria-checked={isPurchased}>
         <input
           type="checkbox"
           id={name}
