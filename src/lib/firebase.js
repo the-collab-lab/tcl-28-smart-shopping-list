912188d7d79b10afefd7c255d940cb80cc7f9a6d
Hulya Karakaya <hulyakarakayaa06@gmail.com>
2021-09-12 12:19:05 -0700

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,19 +1,26 @@
 // NOTE: import only the Firebase modules that you need in your app... except
 // for the second line, which makes both the linter and react-firebase happy
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Initalize Firebase.
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOq3qrkFz2XJK_-2kiUB-G3bhm1DxxLHo',
-  authDomain: 'tcl-28-shopping-list.firebaseapp.com',
-  projectId: 'tcl-28-shopping-list',
-  storageBucket: 'tcl-28-shopping-list.appspot.com',
-  messagingSenderId: '499603825685',
-  appId: '1:499603825685:web:e1385df4cdcd43ec1a9a1c',
+  apiKey: "AIzaSyDNx9AlpM8QdzSWBu-KECVYMaT5UA3rBR0",
+  authDomain: "smart-shopping-list-8b219.firebaseapp.com",
+  projectId: "smart-shopping-list-8b219",
+  storageBucket: "smart-shopping-list-8b219.appspot.com",
+  messagingSenderId: "860567892922",
+  appId: "1:860567892922:web:1e1ba49dd56fabcfc06383",
+  measurementId: "G-GG2J7EM9JD"
 };
 
-let fb = firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const firestore = firebase.firestore();
 
